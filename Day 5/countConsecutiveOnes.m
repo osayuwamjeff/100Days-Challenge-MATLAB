f6b976177f40c874b3c7f9cfd9a1534b0930089c
Jxentech <113486736+osayuwamjeff@users.noreply.github.com>
2025-01-16 15:19:50 +0100

    Day 2 - Day 8 added

--- Day 5/countConsecutiveOnes.m
+++
@@ -1,7 +0,0 @@
-function count = countConsecutiveOnes(vector)
-    % Check for consecutive ones using logical indexing
-    consecutiveOnes = (vector(1:end-1) == 1) & (vector(2:end) == 1);
-    
-    % Count the occurrences
-    count = sum(consecutiveOnes);
-end
